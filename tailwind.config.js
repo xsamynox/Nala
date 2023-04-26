79fac4733f743297305434d3048ed27e779d7dcb
Samantha Moreno <xsamynox@gmail.com>
2023-04-26 16:34:53 -0400

    feat(package): more tools were added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class",
   theme: {
     extend: {},
   },
